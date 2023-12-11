da43e88452dcddb486e8ac5dd1730321075ba618
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2023-12-11 18:28:23 +0000

    commit

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,15 @@
+let name= "my name is mohit"
+
+console.log(name);
+let userone={
+    email:"mohitchawda@gmail.com",
+    upi:"mohit"
+}
+
+let usertwo={
+    email:"user2@gmail.com",
+    upi:"user2name"
+}
+
+console.log(userone.email);
+console.log(usertwo.email);
---
+++ 01_basics/strings.js
@@ -0,0 +1,28 @@
+const name="mohit"
+const repoCount= 50
+
+console.log(name+ repoCount +"value");
+
+console.log(`my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName =new String('mohit')
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('t'));
+
+const newstring= gameName.substring(0,4)
+console.log(newstring);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newstringone= "hitesh"
+console.log(newstringone.trim())
+
+
+const url="hhtps://www.google.com"
