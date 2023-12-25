8ad3a1371082c6df435d327d371208e3d75aed06
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2023-12-25 16:49:03 +0000

    commit

---
+++ 03_basics/iife.js
@@ -0,0 +1,13 @@
+// function hello(){
+//     console.log("hello");
+// }()      =>this will give error:
+
+
+(function hello(){
+    console.log("hello");
+})();                        // now this will run and this is called IIFE...
+
+( () =>{
+    console.log("mohit");
+} )();                      // this is how we also create an arrow function too.....
+
