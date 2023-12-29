31900a1c3e1b656b313138f473f9d2818587740d
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2023-12-29 09:42:10 +0000

    commit

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,9 +1,9 @@
-// for of 
+//for of 
 
-// let arr=[1,23,,4,5,7,3]   //except an array it will run on strings also
-// for (const i of arr) {
-//     console.log(i);
-// }   
+let arr=[1,23,,4,5,7,3]   //except an array it will run on strings also
+for (const i of arr) {
+    console.log(i);
+}   
 
 //maps
 
@@ -12,6 +12,29 @@ map.set("IN","india")
 map.set("us","usa")
 map.set("fr","france")
 
+
+//printing map value or keys value using for of loop
+
 for (const [key,value] of map) {
     console.log(key+":-"+value);
 }
+
+//for in loop...
+
+const MyOBject={
+    js:"javascript",
+    py:"python",
+    r:"reactjs"
+}
+
+for (const key in MyOBject) {
+    console.log(`${key}:${MyOBject[key]}`);
+    }
+
+//forEach loop..
+
+const array=["py","saf","faaf","qw","sda"]
+
+array.forEach(element => {    ////=> this function is called calledback function
+    console.log(element);
+});
