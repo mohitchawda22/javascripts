ce10dd7b3559893025cf188a79efaca3e41c61a9
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2024-01-11 07:31:22 +0000

    commit

---
+++ 10_classed_and_oop/notes.md
@@ -0,0 +1,25 @@
+# javaScript and classes
+
+## OOP
+
+## object
+- a collection of properties (attributes) and methods(functions).
+-toLowerCase
+
+## why use OOP
+
+## parts of OOP
+object Literal
+
+-constructor fuction
+-prototypes
+-classes
+-instances
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+polymorphism
+
---
+++ 10_classed_and_oop/oops.js
@@ -0,0 +1,38 @@
+const user={
+    username:"mohit",
+    loginCount:0,
+    signedIn:true,
+
+    getUSerDetails:function(){
+        // console.log("gpt user detail from database");
+        console.log(`username${this.username}`);
+    }
+
+    
+}
+
+// console.log(user.username);
+// console.log(user.getUSerDetails());
+
+
+// const promiseOne=new Promise()
+// const date= new Date()
+
+function User(username,loginCount,isLoggedIn){
+    this.username=username
+    this.loginCount=loginCount
+    this.isLoggedIn=isLoggedIn
+    
+    this.greeting=function(){
+        console.log(`welcome${this.username}`);
+    }
+
+    return this
+}
+
+const user1=new User("mohit",12,"true")
+const user2=new User("chai",13,false)
+console.log(user1.greeting()) 
+console.log(user1)
+console.log(user2);
+
